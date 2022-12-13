c8421ce19bcc8725b7982c85c07f863b460e14b8
Gabriel Tremblay <gabet4@hotmail.com>
2022-12-13 09:39:45 -0500

    Validation des données entrées lors de la création d'un joueur

--- Forms/FormAjouterJoueur.Designer.cs
+++ Forms/FormAjouterJoueur.Designer.cs
@@ -41,6 +41,12 @@
             this.txtTaille = new System.Windows.Forms.TextBox();
             this.cbPosition = new System.Windows.Forms.ComboBox();
             this.btnAjouter = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Numéro
@@ -160,12 +166,73 @@
             this.btnAjouter.TabIndex = 19;
             this.btnAjouter.Text = "Ajouter";
             this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(215, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(215, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(215, 174);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(215, 236);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(215, 291);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(215, 350);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "label11";
             // 
             // FormAjouterJoueur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 576);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnAjouter);
             this.Controls.Add(this.cbPosition);
             this.Controls.Add(this.txtMasse);
@@ -201,5 +268,11 @@
         private System.Windows.Forms.TextBox txtTaille;
         private System.Windows.Forms.ComboBox cbPosition;
         private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
--- Forms/FormAjouterJoueur.cs
+++ Forms/FormAjouterJoueur.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 using EquipeFrance.Classes;
 
 namespace EquipeFrance.Classes
@@ -19,6 +20,126 @@ namespace EquipeFrance.Classes
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Remettre lels labels à ""
+        public void InitialiserLabel()
+        {
+            label6.Text = label7.Text = label8.Text = label9.Text = label10.Text = label11.Text = "";
+        }
+
+
+        //Vérifier si les valeurs entrés sont acceptables sinon afficher un message d'erreur
+        public bool VerifierRegex(string modele, TextBox tb, Label lb, string messageErreur) //Code tirer du corrigé electeur
+        {
+            //Créer du Regex reg
+            Regex Reg = new Regex(modele);
+            //Vérifier la correspondance entre le champ entré dans le textbox et le modèle
+            if (!Reg.IsMatch(tb.Text))
+            {
+                //Afficher le message d'erreur
+                lb.ForeColor = Color.Red;
+                lb.Text = messageErreur;
+                //Effacer la Textbox
+                tb.Clear();
+                return false;
+            }
+            else lb.Text = ""; //Effacer le label si le cchamp entré est valide
+            return true;
+        }
+
+        /// <summary>
+        /// Vérifier que la valeur entrée est entre 150cm(4,9 pieds) et 220(7,2 pieds)
+        /// </summary>
+        /// <param name="tb"></param>
+        /// <param name="lb"></param>
+        /// <returns></returns>
+        public bool VerifierTaille(TextBox tb, Label lb)
+        {
+            //Convertir le txtbox en int
+            int taille = int.Parse(tb.Text);
+            if (taille >= 150 && taille <= 220) { lb.Text = ""; return true; }
+            else //Sinon afficher un message d'erreur
+            {
+                lb.ForeColor = Color.Red; lb.Text = "Taille doit être entre 150cm et 220cm";
+                tb.Clear();
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Vérifier que la valeur entrée est entre 130lbs(59 kg) et 220lbs(100kg)
+        /// </summary>
+        /// <param name="tb"></param>
+        /// <param name="lb"></param>
+        /// <returns></returns>
+        public bool VerifierMasse(TextBox tb, Label lb)
+        {
+            //Convertir le txtbox en int
+            int masse = int.Parse(tb.Text);
+            if (masse >= 130 && masse <= 220) { lb.Text = ""; return true;}
+            else //Sinon afficher un message d'erreur
+            {
+                lb.ForeColor = Color.Red; lb.Text = "Masse doit être entre 130lbs et 220lbs";
+                tb.Clear();
+                return false;
+            }
+        }
+
+
+
+        //Vérifier le combobox Position
+        public bool VerifierCombobox(ComboBox cb, Label lb)
+        {
+            //Si l'utilisateur a choisi une valeur dans la liste
+            if (cb.Text != "") { lb.Text = ""; return true; }
+            else //Sinon afficher un message d'erreur
+            {
+                lb.ForeColor = Color.Red; lb.Text = "Entrez une position"; return false;
+            }
+        }
+
+        public bool VerifierTous()
+        {
+            //Déclaratiion des booléens de validation des informations entrées
+            bool b_numero, b_nom, b_prenom, b_taille, b_masse, b_combobox;
+            //Appel de la méthode VerifierRegex pour tous les champs et récupération des valeurs retournées dans les variables booléenes
+            b_numero = VerifierRegex("^[0-9]{1,2}$", txtNum, label6, "Un ou deux chiffres");
+            b_nom = VerifierRegex("^[A-Z]{1}[a-z]{1,20}$", txtNom, label7, "Lettre majuscule suivie de 1 à 20 lettres minuscules");
+            b_prenom = VerifierRegex("^[A-Z]{1}[a-z]{1,20}$", txtPrenom, label8, "Lettre majuscule suivie de 1 à 20 lettres minuscules");
+            //appel de la méthode pour vérifier taille
+            b_taille = VerifierTaille(txtTaille, label9);
+            //appel de la méthode pour vérifier masse
+            b_masse = VerifierMasse(txtMasse, label10);
+            //Vérifier le combobox (position)
+            b_combobox = VerifierCombobox(cbPosition, label11);
+            //Si toutes les vérifications sont valides valides retouner vrai
+            if (b_numero && b_nom && b_prenom && b_taille && b_masse && b_combobox)
+                return true;
+            else return false; //sinon retourner faux
+
+        }
+
+        //Remettre tous les champs à défault
+        public void InitialiserControles()
+        {
+            //Réinitialiser tous les champs du formulaire
+            txtNum.Text = "";
+            txtNom.Text = "";
+            txtPrenom.Text = "";
+            txtTaille.Text = "";
+            txtMasse.Text = "";
+            cbPosition.SelectedIndex = -1;
+        }
+
+
+
+
+
+        private void btnAjouter_Click(object sender, EventArgs e)
         {
 
         }
